dc39c1daf7efb675c6567b996ac8d76d0d96563e
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-07-04 13:07:10 -0400

    Implement traits on refs

--- src/storage.rs
+++ src/storage.rs
@@ -12,14 +12,71 @@ pub trait StorageRead<K>: Storage {
 }
 
 pub trait StorageWrite<K>: Storage {
-    fn insert<T: Serialize + DeserializeOwned>(
+    fn insert<V: Serialize + DeserializeOwned>(
         &mut self,
         key: &K,
-        value: &T,
-    ) -> Result<Option<T>, Self::Error>;
-    fn remove<T: DeserializeOwned>(&mut self, key: &K) -> Result<Option<T>, Self::Error>;
+        value: &V,
+    ) -> Result<Option<V>, Self::Error>;
+    fn remove<V: DeserializeOwned>(&mut self, key: &K) -> Result<Option<V>, Self::Error>;
 }
 
 pub trait StorageTemp<K>: Storage {
     fn ttl(&self, key: &K) -> Result<Duration, Self::Error>;
+}
+
+impl<'a, T> Storage for &'a T
+where
+    T: Storage,
+{
+    type Error = T::Error;
+}
+
+impl<'a, T> Storage for &'a mut T
+where
+    T: Storage,
+{
+    type Error = T::Error;
+}
+
+impl<'a, K, T> StorageRead<K> for &'a T
+where
+    T: StorageRead<K>,
+{
+    fn get<V: DeserializeOwned>(&self, key: &K) -> Result<Option<V>, Self::Error> {
+        <T as StorageRead<K>>::get(self, key)
+    }
+
+    fn exists(&self, key: &K) -> Result<bool, Self::Error> {
+        <T as StorageRead<K>>::exists(self, key)
+    }
+}
+
+impl<'a, K, T> StorageRead<K> for &'a mut T
+where
+    T: StorageRead<K>,
+{
+    fn get<V: DeserializeOwned>(&self, key: &K) -> Result<Option<V>, Self::Error> {
+        <T as StorageRead<K>>::get(self, key)
+    }
+
+    fn exists(&self, key: &K) -> Result<bool, Self::Error> {
+        <T as StorageRead<K>>::exists(self, key)
+    }
+}
+
+impl<'a, K, T> StorageWrite<K> for &'a mut T
+where
+    T: StorageWrite<K>,
+{
+    fn insert<V: Serialize + DeserializeOwned>(
+        &mut self,
+        key: &K,
+        value: &V,
+    ) -> Result<Option<V>, Self::Error> {
+        <T as StorageWrite<K>>::insert(self, key, value)
+    }
+
+    fn remove<V: DeserializeOwned>(&mut self, key: &K) -> Result<Option<V>, Self::Error> {
+        <T as StorageWrite<K>>::remove(self, key)
+    }
 }
